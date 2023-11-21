a7fc0fbbfff25fc6ba72c0e697a004c76553e323
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-11-21 18:01:31 +0100

    diseños del home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,18 @@
+import { NavLink } from "react-router-dom"
+import styles from './Home.module.css'
+
 function Home () {
     return (
-        <h1>Home</h1>
+        <div className={`${styles.body}`}>
+            <img src="../../../assets/logotitulo-removebg-preview.png" alt="Code 4 People" />
+            <p className={`${styles.title}`}>A TU ALCANCE</p>
+            <p className={`${styles.text}`}>Somos una compañia dedicada a ayudar a las empresas a lograr su objetivo de digitalizarse, y con el beneficio de que sea gratuito. Esto lo lorgamos gracias a que contamos con la colaboracion de estudiantes y profesionales del rubro.</p>
+            <div className={`${styles.button}`}>
+                <NavLink to="/quienes-somos">
+                    <div>DESCUBRINOS</div>
+                </NavLink>
+            </div>
+        </div>
     )
 }
 
