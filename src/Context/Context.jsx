0cde29b0f8e9b6af50a9ef07a4bcd417dd0b5a5c
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-11-26 20:08:31 +0100

    estilos y logica de pagina registro

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -5,21 +5,60 @@ const MyContext = createContext();
 // eslint-disable-next-line react/prop-types
 const MyContextProvider = ({children}) =>{
 
+    const Users= [{}];
+
     const [user, setUser] = useState('');
+    const [newUser, setNewUser] = useState({
+        nombre: '',
+        email: '',
+        pass: '',
+        tipo_usuario: 0,
+    })
 
     const buttonToRender = user ? "MI CUENTA" : "REGISTRATE AHORA"
 
     const handleNameChange = (event) =>{
         setUser(event.target.value)
-      }
+    }
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setNewUser({...newUser,[name]: value});
+      };
 
+    const saveNewUser = async () =>{
+        // try {
+        //     const response = await fetch('http://localhost:5000/users',{
+        //         method: 'POST',
+        //         headers:{
+        //             'Content-Type': 'application/json',
+        //         },
+        //         body: JSON.stringify(newUser),
+        //     })
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
+        try {
+            Users.push(newUser);
+            console.log(Users[1]);
+            setNewUser({
+                nombre: '',
+                email: '',
+                pass: '',
+                tipo_usuario: 0,
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
         <MyContext.Provider
             value={{
                 user,
-                setUser,
+                newUser,
                 buttonToRender,
-                handleNameChange
+                handleNameChange,
+                handleInputChange,
+                saveNewUser
             }}
         >
             {children}
