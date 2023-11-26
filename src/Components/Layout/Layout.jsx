e583b2ed6faa7df96ed764c4c341e8598f72c1e6
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-11-26 17:53:56 +0100

    Arreglos del background

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -4,8 +4,14 @@ import styles from './Layout.module.css'
 // eslint-disable-next-line react/prop-types
 function Layout({children}) {
 
+  const imageToRender = () =>{
+    if(location.pathname === '/'){
+      return `${styles.backgroundHome}`
+    }
+  }
+
     return (
-      <div className={`${styles.container}`}>
+      <div className={`${styles.container} ${imageToRender()}`}>
          { children }
       </div>
     )
