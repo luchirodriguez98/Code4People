fdc430b5bc1d794c2dc23c5cf67f8f6402ae71e8
LuisHenaoS <luiismiiguelhenaosaldarriiaga@gmail.com>
2023-12-04 20:23:37 +0100

    Middleware log

--- BACK/server.js
+++ BACK/server.js
@@ -16,6 +16,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 
+app.use((req, res, next) => {
+  console.log(`Solicitud recibida: ${req.method} ${req.url}`);
+  next();
+});
 app.use('*', error404);
 app.use(errorHandler);
 
