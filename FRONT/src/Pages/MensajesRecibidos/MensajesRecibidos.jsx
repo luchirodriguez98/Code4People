67fc047b48380ecf3ec5aa3f98e22315a2230b1e
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-12-04 10:11:43 +0100

    inicio en los estilos de la seccion de mensajes recibidos

--- FRONT/src/Pages/MensajesRecibidos/MensajesRecibidos.jsx
+++ FRONT/src/Pages/MensajesRecibidos/MensajesRecibidos.jsx
@@ -1,6 +1,37 @@
+import styles from './MensajesRecibidos.module.css'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+
 function MensajesRecibidos () {
   return (
-        <h1>Mensajes Recibidos</h1>
+        <div className={styles.body}>
+          <div className={styles.backNav}>
+            <ChevronLeftIcon className={styles.icon}/>
+            <p>Mensajes</p>
+          </div>
+          <h1 className={styles.title}>Mensajes recibidos</h1>
+          <div className={styles.messageList}>
+            <span className={styles.messageItem}>
+              <p>USUARIO123</p>
+              <p>Hola! Vi que me seleccionaste para realizar el proyecto</p>
+              <ChevronRightIcon className={styles.icon}/>
+            </span>
+            <span className={styles.messageItem}>
+            <p>USUARIO123</p>
+              <p>Hola! Vi que me seleccionaste para realizar el proyecto</p>
+              <ChevronRightIcon className={styles.icon}/>
+            </span>
+            <span className={styles.messageItem}>
+            <p>USUARIO123</p>
+              <p>Hola! Vi que me seleccionaste para realizar el proyecto</p>
+              <ChevronRightIcon className={styles.icon}/>
+            </span>
+            <span className={styles.messageItem}>
+              <p>USUARIO123</p>
+              <p>Hola! Vi que me seleccionaste para realizar el proyecto</p>
+              <ChevronRightIcon className={styles.icon}/>
+            </span>
+          </div>
+        </div>
   )
 }
 
