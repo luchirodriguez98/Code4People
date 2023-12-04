661f9036b5854f291ea2f7bbd9debdc42d976df9
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-12-04 20:33:49 +0100

    cambios en el background del layout

--- FRONT/src/Components/Layout/Layout.jsx
+++ FRONT/src/Components/Layout/Layout.jsx
@@ -2,16 +2,8 @@ import styles from './Layout.module.css'
 
 // eslint-disable-next-line react/prop-types
 function Layout ({ children }) {
-  const imageToRender = () => {
-    if (location.pathname === '/') {
-      return `${styles.backgroundHome}`
-    } else if (location.pathname === '/quienes-somos') {
-      return `${styles.backgroundQuienesSomos}`
-    }
-  }
-
   return (
-      <div className={`${styles.container} ${imageToRender()}`}>
+      <div className={styles.container}>
          { children }
       </div>
   )
