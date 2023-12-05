ff5588beab9f46e1fc88b42b3d6693795a2b59a8
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-12-05 13:52:19 +0100

    cambios en los estilos del formulario

--- FRONT/src/Pages/Registro/Registro.jsx
+++ FRONT/src/Pages/Registro/Registro.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import { useForm } from '../../Hooks/useForm'
 import styles from './Registro.module.css'
+import stylesForm from '../../Styles/form.module.css'
 
 function Registro () {
   const [formValues, setFormValues, handleFormChange] = useForm({
@@ -12,7 +13,7 @@ function Registro () {
   const { nombre, email, pass } = formValues
 
   const saveNewUser = async (event) => {
-    // event.preventDefault()
+    event.preventDefault()
     if (nombre === '' || email === '' || pass === '') return
 
     const options = {
@@ -41,8 +42,8 @@ function Registro () {
   }
   return (
         <div className={`${styles.body}`}>
-            <form action="" className={`${styles.form}`} onSubmit={(e) => e.preventDefault()}>
-                <h1 className={`${styles.title}`}>Registra tu empresa!</h1>
+            <h1 className={`${styles.title}`}>Registra tu empresa!</h1>
+            <form action="" className={`${stylesForm.form}`} onSubmit={saveNewUser}>
                 <label htmlFor="nombre">NOMBRE / NOMBRE DE EMPRESA</label>
                 <input required
                 type="text"
@@ -73,7 +74,7 @@ function Registro () {
                 onChange={handleFormChange}
                 />
                 <NavLink to="/">
-                  <button className={`${styles.button}`} onClick={() => saveNewUser()}>GUARDAR</button>
+                  <button className={`${stylesForm.button}`}>GUARDAR</button>
                 </NavLink>
             </form>
         </div>
