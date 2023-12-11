adcc63d417f0197e3c739f239a8419b12c654122
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-12-11 15:51:07 +0100

    se añaden los estilos para las secciones de usuarios y proyectos de admin

---
+++ FRONT/src/Components/ListaAdmin/ListaAdmin.jsx
@@ -0,0 +1,24 @@
+import styles from './ListaAdmin.module.css'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+
+function ListaAdmin ({ toMap, toDelete }) {
+  return (
+    <div className={styles.adminList}>
+      {toMap.map(item => {
+        return (
+          <span key={item.id_usuario ? item.id_usuario : item.id_proyecto} className={styles.item}>
+            <div className={styles.text}>
+              <p>{item.nombre ? item.nombre : item.titulo}</p>
+              <p>{item.email ? item.email : item.url}</p>
+            </div>
+            <div onClick={() => toDelete(item.id_usuario ? item.id_usuario : item.id_proyecto)}>
+              <XMarkIcon className={styles.icon}/>
+            </div>
+          </span>
+        )
+      })}
+      </div>
+  )
+}
+
+export { ListaAdmin }
