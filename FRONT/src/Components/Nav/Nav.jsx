3791c530c12ecce69ae9fde725f15ac38afa3527
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-12-12 10:39:38 +0100

    correciones de diseño

--- FRONT/src/Components/Nav/Nav.jsx
+++ FRONT/src/Components/Nav/Nav.jsx
@@ -43,14 +43,14 @@ const Nav = () => {
                         <p>QUIENES SOMOS</p>
                     </NavLink>
                 </li>
-                <li className={`${styles.listItem}`}>
+                {/* <li className={`${styles.listItem}`}>
                     <NavLink
                         to="/planes"
                         className={location === '/planes' ? `${styles.navLinkActive}` : `${styles.navLink}`}
                     >
                         <p>PLANES</p>
                     </NavLink>
-                </li>
+                </li> */}
                 <li className={`${styles.listItem}`}>
                     <NavLink
                         to="/proyectos"
