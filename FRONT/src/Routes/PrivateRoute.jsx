f6fb79ce96a2151bbd36410febbee7c4b0298447
luchirodriguez98 <luchirodriguez98@gmail.com>
2023-12-14 10:31:41 +0100

    correcion en rutas privadas

--- FRONT/src/Routes/PrivateRoute.jsx
+++ FRONT/src/Routes/PrivateRoute.jsx
@@ -1,15 +1,19 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUserContext } from '../Hooks/useUserContext.js'
 
-function PrivateRoute ({ hasRole: role = 'usuario', path = '/' }) {
-  const { usuario } = useUserContext()
-
+function PrivateRoute ({ hasRole: role, path = '/' }) {
+  const { usuario, logOut } = useUserContext()
   const isExpired = usuario?.exp < Date.now()
 
   if (!usuario || isExpired) {
+    logOut()
     return <Navigate to={path || '/login'} />
   }
-  if (usuario.role !== role) {
+  if (Array.isArray(role)) {
+    if (!role.includes(usuario.role)) {
+      return <Navigate to={path} />
+    }
+  } else if (usuario.role !== role) {
     return <Navigate to={path} />
   }
 
