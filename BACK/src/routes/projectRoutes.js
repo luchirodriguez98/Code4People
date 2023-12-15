15c00d01d37bb329ed72c310deb4b83693b3c7ab
LuisHenaoS <luiismiiguelhenaosaldarriiaga@gmail.com>
2023-12-15 20:39:46 +0100

    cambios en router

--- BACK/src/routes/projectRoutes.js
+++ BACK/src/routes/projectRoutes.js
@@ -22,13 +22,13 @@ const projectRouter = express.Router();
 //POST
 projectRouter.post('/nuevoProyecto', userAuth, addProyectoNuevo);
 projectRouter.post('/nuevoAcabado',userAuth,addProyectoAcabado);
-projectRouter.post('/nuevaPeticion/:proyectoId',userAuth,addPeticion);
+projectRouter.post('/nuevaPeticion/:proyectoId',userAuth, addPeticion);
 //GET
-projectRouter.get('/proyectospendientes', userAuth,getAllProyectoARealizar);
-projectRouter.get('/proyectospendientes/:proyectoId', userAuth,getAllProyectoARealizarbyId);
+projectRouter.get('/proyectospendientes', userAuth, isUsuario, getAllProyectoARealizar);
+projectRouter.get('/proyectospendientes/:proyectoId', userAuth, isUsuario, getAllProyectoARealizarbyId);
 projectRouter.get('/proyectos', getAllProyectosAcabados);
-projectRouter.get('/peticiones', userAuth,getPeticiones);
-projectRouter.get('/peticiones/:authorId', userAuth,getPeticionesUser);
+projectRouter.get('/peticiones', userAuth, isEmpresa, getPeticiones);
+projectRouter.get('/peticiones/:authorId', userAuth, isEmpresa, getPeticionesUser);
 
 
 export { projectRouter }
