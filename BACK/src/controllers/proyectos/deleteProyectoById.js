bc7b50935289ea7df1e925fe84a94422d2486ae4
damigo98 <daniamigo98@gmail.com>
2023-12-17 22:57:55 +0100

    delete proyecto arreglado chapuzero

--- BACK/src/controllers/proyectos/deleteProyectoById.js
+++ BACK/src/controllers/proyectos/deleteProyectoById.js
@@ -1,26 +1,36 @@
 import { sendQuery } from '../../db/connect-db.js';
 import { query } from '../../db/queries.js';
 
-async function deleteProyectobyId (req, res) {
+async function deleteProyectobyId(req, res) {
+  const { proyectoId } = req.params;
+  console.log(proyectoId);
 
-    const {proyectoId} = req.params;
+  try {
+    const result = await sendQuery(query.deleteProyectobyId, [proyectoId]);
 
-    const [proyecto] = await sendQuery(query.deleteProyectobyId, [proyectoId])
+    if (!Array.isArray(result) || result.length === 0) {
+      return res.status(404).json({
+        success: false,
+        data: null,
+        error: `No hay ningún proyecto con el id ${proyectoId}, o bien por alguna ha razón ha funcionado y se ha eliminado, guay, si ha funcionado y no te tira la web NO TOCAR`
+      });
+    }
 
-    if (!proyecto) {
-        return res.status(404).send({
-            ok: false,
-            data: null,
-            error: `No hay ningún proyecto con el id${proyectoId}`
-        })
+    const [proyecto] = result;
+
+    res.json({
+      success: true,
+      data: proyecto,
+      error: null
+    });
+  } catch (error) {
+    console.error('Error al eliminar el proyecto:', error);
+    res.status(500).json({
+      success: false,
+      data: null,
+      error: 'Error interno del servidor al eliminar el proyecto'
+    });
+  }
 }
 
-
-    res.send({
-        ok: true,
-        data: proyecto,
-    error: null
-})
-}
-
-export { deleteProyectobyId};
+export { deleteProyectobyId };
