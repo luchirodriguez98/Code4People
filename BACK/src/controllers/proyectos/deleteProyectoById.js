cad206c0824c645835155cb1994480ae2efd1365
damigo98 <daniamigo98@gmail.com>
2023-12-18 10:21:46 +0100

    Porfavor que funcione lpm

--- BACK/src/controllers/proyectos/deleteProyectoById.js
+++ BACK/src/controllers/proyectos/deleteProyectoById.js
@@ -9,20 +9,20 @@ async function deleteProyectobyId(req, res) {
     const result = await sendQuery(query.deleteProyectobyId, [proyectoId]);
 
     if (!Array.isArray(result) || result.length === 0) {
-      return res.status(404).json({
+      res.status(404).json({
         success: false,
         data: null,
-        error: `No hay ningún proyecto con el id ${proyectoId}, o bien por alguna ha razón ha funcionado y se ha eliminado, guay, si ha funcionado y no te tira la web NO TOCAR`
+        error: `No hay ningún proyecto con el id ${proyectoId}, o bien por alguna razón ha funcionado y se ha eliminado, guay, si ha funcionado y no te tira la web NO TOCAR`
+      });
+    } else {
+      const [proyecto] = result;
+
+      res.json({
+        success: true,
+        data: proyecto,
+        error: null
       });
     }
-
-    const [proyecto] = result;
-
-    res.json({
-      success: true,
-      data: proyecto,
-      error: null
-    });
   } catch (error) {
     console.error('Error al eliminar el proyecto:', error);
     res.status(500).json({
