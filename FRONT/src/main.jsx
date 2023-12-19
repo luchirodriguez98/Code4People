29ebb113cd7d7e7dc90bc2ff117bd1ad0f728e42
LuisHenaoS <luiismiiguelhenaosaldarriiaga@gmail.com>
2023-12-19 13:57:10 +0100

    .env

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { App } from './Pages/App/App'
 import './Styles/index.css'
 
+console.log('Console en main.js', import.meta.env)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 // <React.StrictMode>
         <App />
